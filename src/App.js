7bc884d3fd382822320d46b51718b2f67d11d683
arielmineros <arielgmineros@gmail.com>
2023-08-27 14:51:48 -0600

    Modulating components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Menu from './components/Menu';
-import SideMenu from './components/SideMenu';
-import Form from './components/Form';
-import ProductPage from './components/productPage/ProductPage';
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "./components/mainMenu/Menu";
+import SideMenu from "./components/mainMenu/SideMenu";
+import Form from "./components/forms/Form.jsx";
+import ProductPage from "./components/productPage/ProductPage";
 
 function App() {
-  return (
-    <div>
-
-      
-      <Menu />
-      <SideMenu />
-      <ProductPage />
-      <Form />
-    </div>
-  );
+    return (
+        <div>
+            <Menu />
+            <SideMenu />
+            <ProductPage />
+            <Form />
+        </div>
+    );
 }
 
 export default App;
