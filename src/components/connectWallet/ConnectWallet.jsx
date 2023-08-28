c87c764cfb9ac57460a40e62f7ab016745211ea5
arielmineros <arielgmineros@gmail.com>
2023-08-27 21:15:40 -0600

    connecting to wallet button

--- src/components/connectWallet/ConnectWallet.jsx
+++ src/components/connectWallet/ConnectWallet.jsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import Web3 from "web3";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import smartContractRecord from "../smartContract/record.json";
 import { type } from "@testing-library/user-event/dist/type";
 
 function ConnectWallet(props) {
     const [Web3, setWeb3] = useState(null);
-    const [account, setAccout] = useState(null);
+    const [account, setAccount] = useState(null);
     const [balance, setBalance] = useState(null);
     const [contract, setContract] = useState(null);
     const [verificationWallet, setVerificationWallet] = useState(false);
-    //const [buttonWallet, setButtonWallet] = useState(false);
+    const [buttonWallet, setButtonWallet] = useState(false);
     const [listarInfo, setListarInfo] = useState([]);
     const MySwal = withReactContent(Swal);
 
@@ -21,7 +22,31 @@ function ConnectWallet(props) {
             const web3Instance = new Web3(window.ethereum);
             setWeb3(web3Instance);
             try {
+                //ask for wallet access
                 await window.ethereum.enable();
+
+                //Get the address of the current account
+                const accounts = await web3Instance.eth.getAccounts();
+                setAccount(accounts[0]);
+
+                // Get amount in the account
+                const balanceWei = await web3Instance.eth.getBalance(
+                    accounts[0]
+                ); // Represents the balance of an acc in wei
+
+                const balanceEth = web3Instance.utils.fromWei(
+                    balanceWei,
+                    "ether"
+                ); //Converts the balance from wei to eth
+                setBalance(balanceEth);
+                setButtonWallet(false);
+
+                const contractInstance = new web3Instance.eth.Contract(
+                    smartContractRecord,
+                    smartContractRecord &&
+                        "0x34D44DBc2c73B0eCb4bC738bfB850f92AaB89ae2"
+                ); //Create an instance
+                setContract(contractInstance);
             } catch (error) {
                 console.log(error);
                 MySwal.fire({
@@ -36,4 +61,3 @@ function ConnectWallet(props) {
     };
 }
 export default ConnectWallet;
-
--- src/components/mainMenu/Menu.jsx
+++ src/components/mainMenu/Menu.jsx
@@ -4,20 +4,67 @@ import "./Menu.css";
 import "./SearchBar";
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
-import ConnectWallet from "../connectWallet/ConnectWallet";
+import Web3 from "web3";
+//import ConnectWallet from "../connectWallet/ConnectWallet";
 import withReactContent from "sweetalert2-react-content";
 import SearchBar from "./SearchBar";
+import smartContractRecord from "../smartContract/record.json";
 // import mainLogo from './blockchain.png'
 
 function Menu() {
-    //const [Web3, setWeb3] = useState(null);
-    //const [account, setAccout] = useState(null);
-    //const [balance, setBalance] = useState(null);
-    //const [contract, setContract] = useState(null);
+    const [web3, setWeb3] = useState(null);
+    const [account, setAccount] = useState(null);
+    const [balance, setBalance] = useState(null);
+    const [contract, setContract] = useState(null);
     const [verificationWallet, setVerificationWallet] = useState(false);
     const [buttonWallet, setButtonWallet] = useState(false);
-    //const [listarInfo, setListarInfo] = useState([]);
-    //const MySwal = withReactContent(Swal);
+    const [listarInfo, setListarInfo] = useState([]);
+    const MySwal = withReactContent(Swal);
+
+    //Function to connect wallet
+    const connectWallet = async () => {
+        //verify if metamask is installed
+        if (typeof window.ethereum !== "undefined") {
+            const web3Instance = new Web3(window.ethereum);
+            setWeb3(web3Instance);
+            try {
+                //ask for wallet access
+                await window.ethereum.enable();
+
+                //Get the address of the current account
+                const accounts = await web3Instance.eth.getAccounts();
+                setAccount(accounts[0]);
+
+                // Get amount in the account
+                const balanceWei = await web3Instance.eth.getBalance(
+                    accounts[0]
+                ); // Represents the balance of an acc in wei
+
+                const balanceEth = web3Instance.utils.fromWei(
+                    balanceWei,
+                    "ether"
+                ); //Converts the balance from wei to eth
+                setBalance(balanceEth);
+                setButtonWallet(false);
+
+                const contractInstance = new web3Instance.eth.Contract(
+                    smartContractRecord,
+                    smartContractRecord &&
+                        "0x34D44DBc2c73B0eCb4bC738bfB850f92AaB89ae2"
+                ); //Create an instance
+                setContract(contractInstance);
+            } catch (error) {
+                console.log(error);
+                MySwal.fire({
+                    icon: "error",
+                    title: "Oops",
+                    text: "Has rechazado la solicitud de conexión de tu wallet",
+                });
+            }
+        } else {
+            setVerificationWallet(false);
+        }
+    };
 
     //Verify if we have a wallet
     useEffect(() => {
@@ -26,6 +73,9 @@ function Menu() {
                 setVerificationWallet(true);
                 setButtonWallet(true);
             }
+            //else {
+            //setButtonWallet(false);
+            //}
         }
         Wallet();
     }, []);
@@ -84,9 +134,13 @@ function Menu() {
                                 <li className="navegador-item">
                                     <>
                                         {buttonWallet ? (
-                                            <button>Conectar billetera</button>
+                                            <button onClick={connectWallet}>
+                                                Conectar wallet
+                                            </button>
                                         ) : (
-                                            <button>Wallet conectada</button>
+                                            <button onClick={connectWallet}>
+                                                Wallet conectada
+                                            </button>
                                         )}
                                     </>
                                 </li>
