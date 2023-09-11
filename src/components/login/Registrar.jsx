b126e5141eab2599e4f0b99be7e11a7ab3e356ec
joel <joelgrandejoel882@gmail.com>
2023-09-11 17:20:29 -0600

    .

--- src/components/login/Registrar.jsx
+++ src/components/login/Registrar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./css/registrar.css";
 import { Outlet, Link } from "react-router-dom";
-// gfhgjkkl
+// gfhgjkkll
 function RegisTro() {
     return (
         <div class="registro">
